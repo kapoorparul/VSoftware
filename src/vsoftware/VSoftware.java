cb882079040fc0f15bba5e3b225c99cddedcc72c
Parul <Parul@192.168.204.1>
2016-02-08 18:38:46 +0530

    new commit

--- src/vsoftware/VSoftware.java
+++ src/vsoftware/VSoftware.java
@@ -16,6 +16,7 @@ public class VSoftware {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("version 1.0");
         // TODO code application logic here
     }
     
